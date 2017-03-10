ac4690733e6492868ce089184b4c12b418056f5f
Felipe Santos <felipefss@gmail.com>
2017-03-10 17:49:00 +0100

    Trying to get IP

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/api/whoami', (req, res) => {
-    console.log(req.ip);
+    console.log(req.headers);
     res.send('Hello') ;
 });
 
