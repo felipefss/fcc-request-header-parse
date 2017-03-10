ea4bf5577dc567eea8abcd7497ccb3c71d30da7c
Felipe Santos <felipefss@gmail.com>
2017-03-10 18:15:31 +0100

    Add function to parse headers.

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const parseHeader = (header) => {
+    const obj = {
+        ipaddress: header['x-forwarded-for']
+    };
+    const firstIndex = header['user-agent'].indexOf('(') + 1;
+    const lastIndex = header['user-agent'].indexOf(')');
+    const software = header['user-agent'].substring(firstIndex, lastIndex);
+    const language = header['accept-language'].split(',')[0];
+
+    obj.software = software;
+    obj.language = language;
+
+    return obj;
+};
+
 app.get('/api/whoami', (req, res) => {
-    console.log(req.headers);
-    res.send('Hello') ;
+    const parsedHeader = parseHeader(req.headers);
+    res.send(parsedHeader) ;
 });
 
 app.listen(PORT, () => {
